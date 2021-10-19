a59e5b40bf0c74be00db698d6f9978bfd68ddd33
Patrick Polanco <patapoldev@gmail.com>
2021-10-18 19:30:01 -0700

    Add files via upload

---
+++ DIscreteMathHW3/Assignment3Pro6.java
@@ -0,0 +1,51 @@
+package hw6;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Assignment3Pro6 
+{
+	public static void main(String[] args) 
+	{
+		Pro6Support support = new Pro6Support();
+		int flowControl = 0;
+
+		int[] storA = new int[10];
+		int[] storB = new int[10];
+		Scanner userIn = new Scanner(System.in);
+		int numHold;
+
+		//Fills arraylists here
+		System.out.println("Populate Set A with 10 unique integers");
+		storA = support.fillSet(storA);
+
+		System.out.println("Populate Set B with 10 unique integers");
+		storB = support.fillSet(storB);
+
+	}
+
+
+		//Q1
+
+
+		//Q2
+
+
+		//Q3
+
+
+		//Q4
+
+
+
+	public int union() 
+	{
+		
+		return 0;
+	}
+
+	public int intersection() 
+	{
+
+		return 0;
+	}
+}
---
+++ DIscreteMathHW3/Pro6Support.java
@@ -0,0 +1,78 @@
+package hw6;
+import java.util.Scanner;
+
+public class Pro6Support 
+{
+	public void Pro6Support() 
+	{
+		//Warning: Null Construction Zone 
+	}
+
+	public int repeatCheck(int numHold, int stor[]) 
+	{
+		int safeToAdd = 0; // if numHold is unique, this var becomes 1
+		
+		for (int i = 0; i < stor.length; i++) 
+		{
+			if (numHold == stor[i]) 
+				break;
+
+			else
+				continue;
+
+		}
+
+		return safeToAdd;
+	}
+
+	public int[] fillSet(int stor[]) 
+	{
+		Scanner userIn = new Scanner(System.in);
+		boolean complete = false; //make while loop that ends when this var is true
+		int numberHold;
+		int index = 0;
+		int safeToAdd = 0;
+
+		//while() starts here
+		while (!complete) 
+		{
+			
+			//Print indexes and arrays
+			for (int i = 0; i < stor.length; i++) 
+				System.out.print(i + ",  ");
+			
+			System.out.println();
+
+			for (int i = 0; i < stor.length; i++)
+				System.out.print(stor[i] + ",  ");
+
+			//appending array
+			System.out.println("enter an integer for index " + index);
+			numberHold = userIn.nextInt();
+
+			safeToAdd = repeatCheck(numberHold, stor);
+
+			if (safeToAdd == 1) 
+			{
+				stor[index] = numberHold;
+				System.out.println("Added!");
+				index++;
+			}
+
+			else
+				System.out.println("Error, try again.");
+
+			//If index is at 10, loop ends. If not, go again
+			if (index == 10) 
+				complete = true;
+
+			else
+				System.out.println();
+
+		}
+
+		return stor;
+	}
+
+
+}
